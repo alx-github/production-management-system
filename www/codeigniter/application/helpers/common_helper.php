73b99fa3de80661c3cb3c7788e9a0492ffc842d6
ALLEXCEED\lamtd <lamtd@allexceed.vn>
2017-07-26 18:21:57 +0700

    #20 apply fix comment 2 update: * remove check in render_radio_html

--- www/codeigniter/application/helpers/common_helper.php
+++ www/codeigniter/application/helpers/common_helper.php
@@ -162,7 +162,7 @@ if ( ! function_exists('render_radio_html'))
 		foreach ($data as $key => $value)
 		{
 			$html .= '<label class="btn btn-default ' . (($key == $selected_value) ? ' active"' : '"') . '>';
-			$html .= '<input type="radio" name="' . $name . '" value="' . $key . '" autocomplete="off" checked>' . $value . '</label>';
+			$html .= '<input type="radio" name="' . $name . '" value="' . $key . '" autocomplete="off">' . $value . '</label>';
 		}
 		$html .= '</div>';
 		return $html;
